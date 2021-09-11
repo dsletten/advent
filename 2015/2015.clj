d404e22f46c7938412ffe8225cd42444617e8bf7
David Sletten <david@bosatsu.net>
2021-09-10 20:06:15 -0400

    Adding Clojure.

---
+++ 2015/2015.clj
@@ -0,0 +1,88 @@
+;;;;
+;;;;
+;;;;   Clojure feels like a general-purpose language beamed back from the near future.
+;;;;   -- Stu Halloway
+;;;;
+;;;;   Name:               2015.clj
+;;;;
+;;;;   Started:            Fri Sep 10 12:25:18 2021
+;;;;
+;;;;   Purpose:
+;;;;   Purpose: Advent of Code 2015
+;;;;   https://adventofcode.com/2015
+;;;;
+;;;;   Notes:
+;;;;
+;;;;
+
+(ns two-thousand-fifteen
+  (:use clojure.test
+        [clojure.pprint :only (cl-format)])
+  (:import))
+
+;;;
+;;;    Day 1
+;;;
+(defn deliver-presents [s]
+  "Determine floor for Santa to deliver presents based on instructions `s`. `(` means up one floor. `)` means down one floor."
+  (reduce (fn [floor ch]
+            (case ch
+              \( (inc floor)
+              \) (dec floor)))
+          0
+          s))
+
+(defn deliver-presents [s]
+  "Determine floor for Santa to deliver presents based on instructions `s`. `(` means up one floor. `)` means down one floor."
+  (loop [i 0
+         floor 0]
+    (if (== i (count s))
+      floor
+      (case (get s i)
+        \( (recur (inc i) (inc floor))
+        \) (recur (inc i) (dec floor)))) ))
+
+(deftest test-deliver-presents ()
+  (is (== (deliver-presents "(())") 0))
+  (is (== (deliver-presents "()()") 0))
+  (is (== (deliver-presents "(((") 3))
+  (is (== (deliver-presents "(()(()(") 3))
+  (is (== (deliver-presents "))(((((") 3))
+  (is (== (deliver-presents "())") -1))
+  (is (== (deliver-presents "))(") -1))
+  (is (== (deliver-presents ")))") -3))
+  (is (== (deliver-presents ")())())") -3)))
+
+;;;
+;;;    At first, it seems more natural to treat the string as a sequence,
+;;;    but there is some awkwardness.
+;;;    
+(defn enter-basement [s]
+  "Determine when (or whether) Santa enters the basement. 1-based index."
+  (loop [ch (first s)
+         s (rest s)
+         floor (case ch \( 1 \) -1)
+         index 1]
+    (cond (== floor -1) index
+          (empty? s) false
+          :else (case (first s)
+                  \( (recur (first s) (rest s) (inc floor) (inc index))
+                  \) (recur (first s) (rest s) (dec floor) (inc index)))) ))
+
+(defn enter-basement [s]
+  "Determine when (or whether) Santa enters the basement. 1-based index."
+  (loop [i 0
+         floor 0]
+    (cond (== floor -1) i
+          (== i (count s)) false
+          :else (case (get s i)
+                  \( (recur (inc i) (inc floor))
+                  \) (recur (inc i) (dec floor)))) ))
+
+(deftest test-enter-basement ()
+  (is (== (enter-basement ")") 1))
+  (is (== (enter-basement "()())") 5))
+  (is (not (enter-basement "()"))))
+
+
+
--- 2015/2015.lisp
+++ 2015/2015.lisp
@@ -6,20 +6,11 @@
 ;;;;   Name:               2015.lisp
 ;;;;
 ;;;;   Started:            Fri Sep 10 11:56:11 2021
-;;;;   Modifications:
 ;;;;
 ;;;;   Purpose: Advent of Code 2015
+;;;;   https://adventofcode.com/2015
 ;;;;
 ;;;;
-;;;;
-;;;;   Calling Sequence:
-;;;;
-;;;;
-;;;;   Inputs:
-;;;;
-;;;;   Outputs:
-;;;;
-;;;;   Example:
 ;;;;
 ;;;;   Notes:
 ;;;;
@@ -35,13 +26,32 @@
 ;;;    Day 1
 ;;;
 (defun deliver-presents (s)
-  (loop with count = 0
+  "Determine floor for Santa to deliver presents based on instructions S. `(` means up one floor. `)` means down one floor."
+  (loop with floor = 0
         for ch across s
-        do (case ch
-             (#\( (incf count))
-             (#\) (decf count))
-             (otherwise (return nil)))
-        finally (return count)))
+        do (ecase ch
+             (#\( (incf floor))
+             (#\) (decf floor)))
+        finally (return floor)))
+
+(defun deliver-presents (s)
+  "Determine floor for Santa to deliver presents based on instructions S. `(` means up one floor. `)` means down one floor."
+  (labels ((deliver (i floor)
+             (if (= i (length s))
+                 floor
+                 (ecase (char s i)
+                   (#\( (deliver (1+ i) (1+ floor)))
+                   (#\) (deliver (1+ i) (1- floor)))) )))
+    (deliver 0 0)))
+
+(defun deliver-presents (s)
+  "Determine floor for Santa to deliver presents based on instructions S. `(` means up one floor. `)` means down one floor."
+  (reduce #'(lambda (floor ch)
+              (ecase ch
+                (#\( (1+ floor))
+                (#\) (1- floor))))
+          s
+          :initial-value 0))
 
 (deftest test-deliver-presents ()
   (check
@@ -56,18 +66,28 @@
    (= (deliver-presents ")())())") -3)))
 
 (defun enter-basement (s)
-  (loop with count = 0
+  "Determine when (or whether) Santa enters the basement. 1-based index."
+  (loop with floor = 0
         for index from 1
         for ch across s
-        do (case ch
-             (#\( (incf count))
-             (#\) (decf count))
-             (otherwise (return nil)))
-        when (= count -1)
-          do (return index)
-        finally (return count)))
+        do (ecase ch
+             (#\( (incf floor))
+             (#\) (decf floor)))
+        when (= floor -1)
+          do (return index)))
+
+(defun enter-basement (s)
+  "Determine when (or whether) Santa enters the basement. 1-based index."
+  (labels ((basement (i floor)
+             (cond ((= floor -1) i)
+                   ((= i (length s)) nil)
+                   (t (ecase (char s i)
+                        (#\( (basement (1+ i) (1+ floor)))
+                        (#\) (basement (1+ i) (1- floor)))) ))))
+    (basement 0 0)))
 
 (deftest test-enter-basement ()
   (check
    (= (enter-basement ")") 1)
-   (= (enter-basement "()())") 5)))
+   (= (enter-basement "()())") 5)
+   (not (enter-basement "()"))))
